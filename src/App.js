1085badae8fd7929c1898cacf98bca177e830f0d
chandratand <Chandra.tand@gmail.com>
2021-11-08 15:48:56 +0700

    Menyiapkan Router dan Utils Colors Dan Fonts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import {Splash} from './page';
+import Router from './router';
 
 const App = () => {
   return (
-    <View>
-      <Text>Hello World</Text>
-    </View>
+    <NavigationContainer>
+      <Router />
+    </NavigationContainer>
   );
 };
 
---
+++ src/utils/colors/index.js
@@ -0,0 +1,12 @@
+const mainColors = {
+  green: '#92C87A',
+  dark: '#212C1C',
+  grey: '#7D8797',
+};
+
+export const colors = {
+  primary: mainColors.dark,
+  secondary: mainColors.grey,
+  tertiary: mainColors.green,
+  white: 'white',
+};
